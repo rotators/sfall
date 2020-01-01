c86b17f7fdb41019102f13ebe64b7ad262e4e1f9
Wipe <wipe.git@gmail.com>
2020-01-01 18:41:29 +0100

    fix namespace

--- sfall/Modules/Rotators.cpp
+++ sfall/Modules/Rotators.cpp
@@ -12,14 +12,14 @@ const char rotatorsIni[] = ".\\ddraw.rotators.ini";
 
 static void InitCustomDll()
 {
-    std::vector<std::string> names = GetIniList( "Debugging", "CustomDll", "", 512, ',', rotatorsIni );
+    std::vector<std::string> names = sfall::GetIniList( "Debugging", "CustomDll", "", 512, ',', rotatorsIni );
 
     for( const auto& name : names )
     {
         if( name.empty() )
             continue;
 
-        dlog_f( "Loading custom dll %s", DL_MAIN, name.c_str() );
+        sfall::dlog_f( "Loading custom dll %s", DL_MAIN, name.c_str() );
 
         LoadLibraryA( name.c_str() );
     }
